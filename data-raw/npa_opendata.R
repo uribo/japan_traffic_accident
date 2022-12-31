371d668c1d384c62f3f88bd9a6aeecf5e5776732
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2023-01-01 08:32:07 +0900

    [modify]条件分岐

--- data-raw/npa_opendata.R
+++ data-raw/npa_opendata.R
@@ -8,42 +8,44 @@
 fs::dir_create(here::here("data-raw/npa"), recurse = TRUE)
 npa_domain <- "https://www.npa.go.jp/"
 
-x <- 
-  sprintf("%spublications/statistics/koutsuu/opendata/index_opendata.html", npa_domain) |> 
-  rvest::read_html()
-
-df_npa_list <- 
-  tibble::tibble(
-  year = x |> 
-    rvest::html_elements(css = "#contArea > main > article > section > section:nth-child(3) > p > a") |> 
-    rvest::html_text() |> 
-    stringi::stri_trans_nfkc() |> 
-    stringr::str_remove("\\(.+\\)") |> 
-    stringr::str_remove("年") |> 
-    readr::parse_number(),
-  url = x |> 
-    rvest::html_elements(css = "#contArea > main > article > section > section:nth-child(3) > p > a") |> 
-    rvest::html_attr(name = "href") |> 
-    xml2::url_absolute(base = npa_domain)) |> 
-  ensurer::ensure(nrow(.) == 3L)
-
-purrr::pwalk(
-  df_npa_list,
-  function(year, url) {
-    fs::dir_create(here::here(glue::glue("data-raw/npa/{year}")))
-    Sys.sleep(8)
-    x <- 
-      url |> 
-      rvest::read_html() |> 
-      rvest::html_elements(css = "#contArea > main > article > section > section > p > a") |> 
-      rvest::html_attr(name = "href") |> 
-      stringr::str_subset(".csv") |> 
-      xml2::url_absolute(base = npa_domain)
+if (length(fs::dir_ls(here::here("data-raw/npa"), recurse = TRUE, regexp = "(hoju|hon|kosoku)hyo_.+.csv$")) != 9L) {
+  x <- 
+    sprintf("%spublications/statistics/koutsuu/opendata/index_opendata.html", npa_domain) |> 
+    rvest::read_html()
+  
+  df_npa_list <- 
+    tibble::tibble(
+      year = x |> 
+        rvest::html_elements(css = "#contArea > main > article > section > section:nth-child(3) > p > a") |> 
+        rvest::html_text() |> 
+        stringi::stri_trans_nfkc() |> 
+        stringr::str_remove("\\(.+\\)") |> 
+        stringr::str_remove("年") |> 
+        readr::parse_number(),
+      url = x |> 
+        rvest::html_elements(css = "#contArea > main > article > section > section:nth-child(3) > p > a") |> 
+        rvest::html_attr(name = "href") |> 
+        xml2::url_absolute(base = npa_domain)) |> 
+    ensurer::ensure(nrow(.) == 3L)
+  
+  purrr::pwalk(
+    df_npa_list,
+    function(year, url) {
+      fs::dir_create(here::here(glue::glue("data-raw/npa/{year}")))
+      Sys.sleep(8)
+      x <- 
+        url |> 
+        rvest::read_html() |> 
+        rvest::html_elements(css = "#contArea > main > article > section > section > p > a") |> 
+        rvest::html_attr(name = "href") |> 
+        stringr::str_subset(".csv") |> 
+        xml2::url_absolute(base = npa_domain)
       download.file(url = x,
                     destfile = here::here(glue::glue("data-raw/npa/{year}/{x}",
                                                      x = basename(x))))
-  }
-)
+    }
+  )
+}
 
 
 # ファイル定義書 -----------------------------------------------------------------
